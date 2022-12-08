b44d7a8b4ea6fac482eba0a1e7c5d2ff5c4bca3d
gdisalvo30 <gdisalvo30@gmail.com>
2022-12-07 20:27:37 -0800

    switching to linux

---
+++ Makefile
@@ -0,0 +1,2 @@
+functionmake: functions.c
+	gcc -shared -o functionslib.dll functions.c
---
+++ functions.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
+void hello_world() {
+    printf("hello world\n");
+}
